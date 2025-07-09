fc48c473182d374ad908677246dc4ef819fca0b5
User <User@DESKTOP-QHG18FL>
2025-07-09 19:24:29 +0300

    Comparison operators DONE

--- IntroductionToOOP/Fraction/main.cpp
+++ IntroductionToOOP/Fraction/main.cpp
@@ -203,8 +203,49 @@ Fraction operator/(const Fraction& left, const Fraction& right)
 	return left * right.inverted();
 }
 
+//							Comparison operators:
+bool operator==(Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	return
+		left.get_numerator()*right.get_denominator() ==
+		right.get_numerator()*left.get_denominator();
+}
+bool operator!=(const Fraction& left, const Fraction& right)
+{
+	return !(left == right);
+}
+bool operator>(Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	return
+		left.get_numerator()*right.get_denominator() >
+		right.get_numerator()*left.get_denominator();
+}
+bool operator<(Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	return
+		left.get_numerator()*right.get_denominator() <
+		right.get_numerator()*left.get_denominator();
+}
+bool operator>=(const Fraction& left, const Fraction& right)
+{
+	return !(left < right);
+	//return left > right || left == right;
+}
+bool operator<=(const Fraction& left, const Fraction& right)
+{
+	return !(left > right);
+	//return left < right || left == right;
+}
+
+
 //#define CONSTRUCTORS_CHECK
-#define ARITHMETICAL_OPERATORS_CHECK
+//#define ARITHMETICAL_OPERATORS_CHECK
 //#define INCREMENTO_DECREMENTO_CHECK
 
 void main()
@@ -271,5 +312,7 @@ cout << i << endl;*/
 	B.print();
 #endif // INCREMENTO_DECREMENTO_CHECK
 
+	cout << (2 == 3) << endl;
+	cout << (Fraction(1, 3) <= Fraction(5, 11)) << endl;
 
 }
