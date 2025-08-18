f59116f1f384c5942667db4a3ac3e2e8538cc8ec
User <User@DESKTOP-QHG18FL>
2025-08-18 20:53:16 +0300

    Print() & Save() DONE

--- Inheritance/Academy/main.cpp
+++ Inheritance/Academy/main.cpp
@@ -257,6 +257,29 @@ public:
 	}
 };
 
+void Print(Human* group[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		group[i]->info(cout);
+		cout << delimiter << endl;
+	}
+	cout << "Количество объектов: " << group[0]->get_count() << endl;
+	cout << "Количество объектов: " << Human::get_count() << endl;
+}
+void Save(Human* group[], const int n, const std::string& filename)
+{
+	std::ofstream fout(filename);
+	for (int i = 0; i < n; i++)
+	{
+		fout << *group[i] << endl;
+	}
+	fout.close();
+	std::string cmd = "notepad ";
+	cmd += filename;
+	system(cmd.c_str());	//Метод c_str() возвращает строку в виде массива символов (char* );
+}
+
 //#define INHERITANCE
 #define POLYMORPHISM //(poly - много, morphis - форма)
 
@@ -303,17 +326,8 @@ void main()
 		new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
 	};
 
-	std::ofstream fout("group.txt");
-	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
-	{
-		group[i]->info(cout);
-		fout << *group[i] << endl;
-		cout << delimiter << endl;
-	}
-	cout << "Количество объектов: " << group[0]->get_count() << endl;
-	cout << "Количество объектов: " << Human::get_count() << endl;
-	fout.close();
-	system("notepad group.txt");
+	Print(group, sizeof(group) / sizeof(group[0]));
+	Save(group, sizeof(group) / sizeof(group[0]), "group.txt");
 
 	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
 	{
